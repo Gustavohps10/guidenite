a292290bd67da8b2174dbf4e971176ed0c60242f
gustavo <gustavoh.santos735@gmail.com>
2022-08-15 03:40:26 -0300

    Added - show and hide window with gamepad guide button

--- main.js
+++ main.js
@@ -1,22 +1,46 @@
 const { app, Menu, Tray, BrowserWindow, globalShortcut, ipcMain, desktopCapturer } = require('electron')
+const {spawn, exec} = require("node:child_process")
+
+let command =  __dirname + "\\button_on_360_guide_v5\\listener.exe"
+
+let child = spawn(command, {windowsHide: false});
+child.stdout.setEncoding('utf8')
 
 let win = null
 const createWindow = () => {
     win = new BrowserWindow({
         fullscreen: true,
-         webPreferences: {
+        webPreferences: {
              nodeIntegration: true,
              contextIsolation: false
         },
         transparent: true,
         frame: false,
-        //resizable: false,
+        resizable: false,
         alwaysOnTop: true
     })
+
+    win.setAlwaysOnTop(true, 'screen');
+    win.setMinimizable(false);
   
     win.loadFile('index.html')
+
+    win.on('show', () => {
+        setTimeout(() => {
+            win.focus();
+        }, 100);
+    });
 }
 
 app.whenReady().then(() => {
     createWindow();
+})
+
+child.stdout.on("data",function(stdout){
+    if(win.isVisible()){
+        win.blur()
+        win.hide()
+    }else{
+        win.show()
+    }
 })
